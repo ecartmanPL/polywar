fef0e994a72886a339f21814dc3b6afe4e2e18bc
cartmanPL <wsiekiera@gmail.com>
2017-07-25 20:37:40 +0200

    spring boot sample app

---
+++ java_app/src/main/java/net/siekiera/polywar/PolywarApplication.java
@@ -0,0 +1,12 @@
+package net.siekiera.polywar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolywarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolywarApplication.class, args);
+	}
+}
